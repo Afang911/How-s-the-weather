5da2bba818f47a3c8962feb3ba8dd9001ee9a485
Austin Fanger <fanger_austin@yahoo.com>
2023-03-26 22:40:00 -0600

    Js variables

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+
+const searchForm = document.getElementById("search-form");
+const cityInput = document.getElementById("city-input");
+const searchHistory = document.getElementById("search-history");
+const currentWeather = document.getElementById("current-weather");
+const forecast = document.getElementById("forecast");
